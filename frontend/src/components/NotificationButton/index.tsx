62f20fdddf82a89e1ca2843e051f1dc30f2a9bcb
EmuladorJonas <jonassantos13@live.com>
2022-07-17 23:33:06 -0300

    First Component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/img/Vector.svg'
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="btn_not">
+            <img src={icon} alt="notifcar" />
+        </div>
+    )
+}
+
+export default NotificationButton
